1f46cf1b0ed15a3bac1e66a3bfcfc8dade6ac47a
TomEganDev <tomegandev@gmail.com>
2025-01-26 01:41:48 +1100

    Projectile tunneling fix + removed jump speed UI, tweaked camera

--- Assets/Scripts/BubbleGun.cs
+++ Assets/Scripts/BubbleGun.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class BubbleGun : MonoBehaviour
 {
@@ -12,7 +13,35 @@ public class BubbleGun : MonoBehaviour
 
         if (Input.GetMouseButtonDown(0)) // fire
         {
+            var filter = new ContactFilter2D(); // todo - actually filter
             FiringPoint.GetPositionAndRotation(out var position, out var rotation);
+            var hitCount = Physics2D.Raycast(transform.position, transform.right, filter, GlobalBuffers.HitBuffer,
+                Vector3.Distance(position, transform.position));
+            Assert.IsTrue(hitCount <= GlobalBuffers.HitBuffer.Length);
+            var closestDistance = float.MaxValue;
+            var closestIndex = -1;
+            for (int i = 0; i < hitCount; i++)
+            {
+                var hit = GlobalBuffers.HitBuffer[i];
+                if (Player.Instance.IsPlayer(hit.collider.gameObject))
+                {
+                    continue;
+                }
+                
+                // todo - extra filtering
+
+                if (hit.distance < closestDistance)
+                {
+                    closestDistance = hit.distance;
+                    closestIndex = i;
+                }
+            }
+
+            if (closestIndex >= 0)
+            {
+                position = GlobalBuffers.HitBuffer[closestIndex].point;
+            }
+                
             var projectile = Instantiate(PreBubblePrefab, position, rotation);
             // Debug.Log($"Shot {projectile.name}", projectile);
             // Debug.Break();
